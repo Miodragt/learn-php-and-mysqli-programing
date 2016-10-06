d6fee2eab55d3ec7aa7f3940155834b87a4efd35
Miodragt <trajanovicm@gmail.com>
2016-10-06 20:57:24 +0200

    Create Welcame.php
    That is start. I wish all the best for all of you.

---
+++ Welcame.php
@@ -0,0 +1,13 @@
+/* I wish learn and teach all around me what I learn. On this way we can to be strog.
+1.first for that we must make data base on the server
+2.next we make table who is need
+3. file connect.php with who we can make connection with our database and tables
+4. Index.php 
+5. admin.php for administration data
+6. delete.php for delete some record
+7. insert.php for inserting record from show on the index.php
+*/
+<?php
+echo" My new peoject, with who we can make more nice stuff";
+echo" Every who wish connect with me YOU WELCAME";
+?>
